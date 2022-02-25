738e88a5494e35eebba171cb7becf03bc7489b64
Maria Mao <maria.mao.csp@gmail.com>
2022-02-25 11:53:08 -0500

    Update TestGhostMove.java
    Added tests

--- Projects/P3/tests/TestGhostMove.java
+++ Projects/P3/tests/TestGhostMove.java
@@ -5,6 +5,22 @@ import java.io.*;
 public class TestGhostMove extends TestCase {
 
 	public void testGhostMove() throws FileNotFoundException{
-	
+		//Creating A Map
+		Mainframe frame = new MainFrame(); //Creates A New Map With Walls and Tokens Initialized
+
+		//Creating Players
+		Ghost ghost = frame.addGhost(new Location(x, y), "name", Color.red); //Creates a red ghost named "name" at location x,y
+
+		//Start The Game
+		frame.startGame();
+		ArrayList<Location> moves = ghost.get_valid_moves();
+		ghost.move();
+		assertTrue(moves.contains(ghost.myLoc));
+
+		moves = ghost.get_valid_moves();
+		ghost.move();
+		assertTrue(moves.contains(ghost.myLoc));
+
+
 	}
 }
