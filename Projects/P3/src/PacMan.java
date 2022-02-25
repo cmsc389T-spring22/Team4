3bacfae29f3a4a8f8c9b2073aed9bbf65604b5ee
Maria Mao <maria.mao.csp@gmail.com>
2022-02-25 11:42:54 -0500

    Update PacMan.java
    pacman-move commit

--- Projects/P3/src/PacMan.java
+++ Projects/P3/src/PacMan.java
@@ -19,7 +19,12 @@ public class PacMan{
 	}
 
 	public boolean move() {
-		return false;
+		if (get_valid_moves().size() == 0) {
+			return false;
+		} else {
+			this.myLoc = get_valid_moves().get(0);
+			return true;
+		}
 	}
 
 	public boolean is_ghost_in_range() { 
