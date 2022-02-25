6033bc7361baec1fed926b9e4952bafe03b1a98b
Maria Mao <maria.mao.csp@gmail.com>
2022-02-25 11:52:32 -0500

    Update TestPacManMove.java
    Added test

--- Projects/P3/tests/TestPacManMove.java
+++ Projects/P3/tests/TestPacManMove.java
@@ -5,6 +5,20 @@ import java.io.*;
 public class TestPacManMove extends TestCase {
 
 	public void testPacManMove() throws FileNotFoundException{
-		return null;
+		//Creating A Map
+		Mainframe frame = new MainFrame(); //Creates A New Map With Walls and Tokens Initialized
+
+		//Creating Players
+		PacMan pacman = frame.addPacMan(new Location(x, y)); //Creates PacMan at location x, y
+
+		//Start The Game
+		frame.startGame();
+		ArrayList<Location> moves = pacman.get_valid_moves();
+		pacman.move();
+		assertTrue(moves.contains(pacman.myLoc));
+
+		moves = pacman.get_valid_moves();
+		pacman.move();
+		assertTrue(moves.contains(pacman.myLoc));
 	}
 }
