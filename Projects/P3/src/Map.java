dd8e0dfdbba10eda1a6a5f10e001da5ecd7bfc00
Maria Mao <maria.mao.csp@gmail.com>
2022-02-25 16:12:34 -0500

    Map-move update and test

--- Projects/P3/src/Map.java
+++ Projects/P3/src/Map.java
@@ -55,7 +55,15 @@ public class Map{
 	public boolean move(String name, Location loc, Type type) {
 		//update locations, components, and field
 		//use the setLocation method for the component to move it to the new location
-		return false;
+
+		if (!locations.containsKey(name) || !field.contains(loc) || !components.containsKey(name)) {
+			return false;
+		} 
+		components.get(name).setLocation(loc.x, loc.y);
+		locations.replace(name, loc);
+		field.replace(loc, emptySet);
+		
+		return true;
 	}
 	
 	public HashSet<Type> getLoc(Location loc) {
--- Projects/P3/tests/TestMapMove.java
+++ Projects/P3/tests/TestMapMove.java
@@ -1,10 +1,22 @@
 import junit.framework.*;
 import java.awt.Color;
 import java.io.*;
+import java.util.ArrayList;
 
 public class TestMapMove {
 
 	public void testMapMove() {
-		return null;
+	//Creating A Map
+	Mainframe frame = new MainFrame(); //Creates A New Map With Walls and Tokens Initialized
+
+	//Creating Players
+	PacMan pacman = frame.addPacMan(new Location(x, y)); //Creates PacMan at location x, y
+
+	//Start The Game
+	frame.startGame();
+
+	ArrayList<Location> locs = pacman.get_valid_moves();
+	
+	assertTrue(frame.move(pacman.name, locs.get(0), Type.PACMAN));
 	}
 }
