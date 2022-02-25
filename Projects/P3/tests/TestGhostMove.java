ddef41c9e3458e0d820154e5c76dfdd05c654152
Maria Mao <maria.mao.csp@gmail.com>
2022-02-25 16:12:54 -0500

    Update TestGhostMove.java

--- Projects/P3/tests/TestGhostMove.java
+++ Projects/P3/tests/TestGhostMove.java
@@ -5,6 +5,7 @@ import java.io.*;
 public class TestGhostMove extends TestCase {
 
 	public void testGhostMove() throws FileNotFoundException{
+<<<<<<< Updated upstream
 		//Creating A Map
 		Mainframe frame = new MainFrame(); //Creates A New Map With Walls and Tokens Initialized
 
@@ -22,5 +23,22 @@ public class TestGhostMove extends TestCase {
 		assertTrue(moves.contains(ghost.myLoc));
 
 
+=======
+	//Creating A Map
+	Mainframe frame = new MainFrame(); //Creates A New Map With Walls and Tokens Initialized
+
+	//Creating Players
+	Ghost ghost = frame.addGhost(new Location(x, y)); //Creates PacMan at location x, y
+
+	//Start The Game
+	frame.startGame();
+	ArrayList<Location> moves = ghost.get_valid_moves();
+	ghost.move();
+	assertTrue(moves.contains(ghost.myLoc));
+
+	moves = ghost.get_valid_moves();
+	ghost.move();
+	assertTrue(moves.contains(ghost.myLoc));
+>>>>>>> Stashed changes
 	}
 }
