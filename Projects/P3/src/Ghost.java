bd304750d78691c4c12a64d57a2e47cae3caa2e7
Soyoung Hong <kirbysoyoung@gmail.com>
2022-03-02 10:22:27 -0500

    ghost - get_valid_moves and test

--- Projects/P3/src/Ghost.java
+++ Projects/P3/src/Ghost.java
@@ -12,8 +12,27 @@ public class Ghost{
 		this.myMap = map;
 	}
 
+	// Checking if adjacent locations are walls
 	public ArrayList<Location> get_valid_moves() {
-		return null;
+		ArrayList<Location> valid_moves = new ArrayList<Location>();
+
+		if (!myMap.getLoc(myLoc.shift(-1, 0)).contains(Map.Type.WALL)){
+			valid_moves.add(myLoc.shift(-1, 0));
+		}
+
+		if (!myMap.getLoc(myLoc.shift(1, 0)).contains(Map.Type.WALL)){
+			valid_moves.add(myLoc.shift(1, 0));
+		}
+
+		if (!myMap.getLoc(myLoc.shift(0, 1)).contains(Map.Type.WALL)){
+			valid_moves.add(myLoc.shift(0, 1));
+		}
+
+		if (!myMap.getLoc(myLoc.shift(0, -1)).contains(Map.Type.WALL)){
+			valid_moves.add(myLoc.shift(0, -1));
+		}
+
+		return valid_moves;
 	}
 
 	public boolean move() {
