c17e749b6237ab5218f907fa545147c308e6d564
Maria Mao <maria.mao.csp@gmail.com>
2022-03-02 14:46:49 -0500

    Updated TestMapMove

--- Projects/P3/tests/TestMapMove.java
+++ Projects/P3/tests/TestMapMove.java
@@ -6,17 +6,20 @@ import java.util.ArrayList;
 public class TestMapMove {
 
 	public void testMapMove() {
-	//Creating A Map
-	Mainframe frame = new MainFrame(); //Creates A New Map With Walls and Tokens Initialized
+		//Creating A Map
+		Mainframe frame = new MainFrame(); //Creates A New Map With Walls and Tokens Initialized
 
-	//Creating Players
-	PacMan pacman = frame.addPacMan(new Location(x, y)); //Creates PacMan at location x, y
+		//Creating Players
+		PacMan pacman = frame.addPacMan(new Location(5, 5), "Pacman", Color.red); //Creates PacMan at location x, y
 
-	//Start The Game
-	frame.startGame();
+		//Start The Game
+		frame.startGame();
 
-	ArrayList<Location> locs = pacman.get_valid_moves();
+		ArrayList<Location> locs = pacman.get_valid_moves();
+
+		if (locs.size() != 0) {
+			assertTrue(frame.move(pacman.name, locs.get(0), Type.PACMAN));
+		}
 	
-	assertTrue(frame.move(pacman.name, locs.get(0), Type.PACMAN));
 	}
 }
