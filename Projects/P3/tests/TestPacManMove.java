398a76c868a8e9e3c2737271c413fbd1eb6565b4
Maria Mao <maria.mao.csp@gmail.com>
2022-03-02 14:48:00 -0500

    Updated TestPacManMove

--- Projects/P3/tests/TestPacManMove.java
+++ Projects/P3/tests/TestPacManMove.java
@@ -9,16 +9,33 @@ public class TestPacManMove extends TestCase {
 		Mainframe frame = new MainFrame(); //Creates A New Map With Walls and Tokens Initialized
 
 		//Creating Players
+<<<<<<< Updated upstream
 		PacMan pacman = frame.addPacMan(new Location(x, y)); //Creates PacMan at location x, y
+=======
+		PacMan pacman = frame.addPacMan(new Location(x, y), "Pacman", Color.red); //Creates PacMan at location x, y
+>>>>>>> Stashed changes
 
 		//Start The Game
 		frame.startGame();
 		ArrayList<Location> moves = pacman.get_valid_moves();
+<<<<<<< Updated upstream
 		pacman.move();
 		assertTrue(moves.contains(pacman.myLoc));
 
 		moves = pacman.get_valid_moves();
 		pacman.move();
 		assertTrue(moves.contains(pacman.myLoc));
+=======
+		if (moves.size() != 0) {
+			pacman.move();
+			assertTrue(moves.contains(pacman.myLoc));
+		}
+
+		moves = pacman.get_valid_moves();
+		if (moves.size() != 0) {
+			pacman.move();
+			assertTrue(moves.contains(pacman.myLoc));
+		}
+>>>>>>> Stashed changes
 	}
 }
