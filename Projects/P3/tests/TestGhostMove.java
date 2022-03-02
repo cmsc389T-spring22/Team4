d81194c7c01b02e3a130ccf2745d011e3c951572
Maria Mao <66149426+maria-mao@users.noreply.github.com>
2022-03-02 14:57:49 -0500

    Update TestGhostMove.java

--- Projects/P3/tests/TestGhostMove.java
+++ Projects/P3/tests/TestGhostMove.java
@@ -5,50 +5,16 @@ import java.io.*;
 public class TestGhostMove extends TestCase {
 
 	public void testGhostMove() throws FileNotFoundException{
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-=======
->>>>>>> Stashed changes
 		//Creating A Map
 		Mainframe frame = new MainFrame(); //Creates A New Map With Walls and Tokens Initialized
 
 		//Creating Players
-<<<<<<< Updated upstream
-		Ghost ghost = frame.addGhost(new Location(x, y), "name", Color.red); //Creates a red ghost named "name" at location x,y
-=======
 		Ghost ghost = frame.addGhost(new Location(5, 5), "Ghost", Color.white); //Creates PacMan at location x, y
->>>>>>> Stashed changes
 
 		//Start The Game
 		frame.startGame();
 		ArrayList<Location> moves = ghost.get_valid_moves();
-<<<<<<< Updated upstream
-		ghost.move();
-		assertTrue(moves.contains(ghost.myLoc));
 
-		moves = ghost.get_valid_moves();
-		ghost.move();
-		assertTrue(moves.contains(ghost.myLoc));
-
-
-=======
-	//Creating A Map
-	Mainframe frame = new MainFrame(); //Creates A New Map With Walls and Tokens Initialized
-
-	//Creating Players
-	Ghost ghost = frame.addGhost(new Location(x, y)); //Creates PacMan at location x, y
-
-	//Start The Game
-	frame.startGame();
-	ArrayList<Location> moves = ghost.get_valid_moves();
-	ghost.move();
-	assertTrue(moves.contains(ghost.myLoc));
-
-	moves = ghost.get_valid_moves();
-	ghost.move();
-	assertTrue(moves.contains(ghost.myLoc));
->>>>>>> Stashed changes
-=======
 		if (moves.size() != 0) {
 			ghost.move();
 		assertTrue(moves.contains(ghost.myLoc));
@@ -59,7 +25,5 @@ public class TestGhostMove extends TestCase {
 			ghost.move();
 		assertTrue(moves.contains(ghost.myLoc));
 		}
-
->>>>>>> Stashed changes
 	}
 }
