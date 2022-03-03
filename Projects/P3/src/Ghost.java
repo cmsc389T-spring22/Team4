b9468e4e17eba78841e0d6163e1680d84978ecee
nehashijo <72104816+nehashijo@users.noreply.github.com>
2022-03-02 23:10:05 -0500

    Revert "ghost - get_valid_moves and test"

--- Projects/P3/src/Ghost.java
+++ Projects/P3/src/Ghost.java
@@ -12,27 +12,8 @@ public class Ghost{
 		this.myMap = map;
 	}
 
-	// Checking if adjacent locations are walls
 	public ArrayList<Location> get_valid_moves() {
-		ArrayList<Location> valid_moves = new ArrayList<Location>();
-
-		if (!myMap.getLoc(myLoc.shift(-1, 0)).contains(Map.Type.WALL)){
-			valid_moves.add(myLoc.shift(-1, 0));
-		}
-
-		if (!myMap.getLoc(myLoc.shift(1, 0)).contains(Map.Type.WALL)){
-			valid_moves.add(myLoc.shift(1, 0));
-		}
-
-		if (!myMap.getLoc(myLoc.shift(0, 1)).contains(Map.Type.WALL)){
-			valid_moves.add(myLoc.shift(0, 1));
-		}
-
-		if (!myMap.getLoc(myLoc.shift(0, -1)).contains(Map.Type.WALL)){
-			valid_moves.add(myLoc.shift(0, -1));
-		}
-
-		return valid_moves;
+		return null;
 	}
 
 	public boolean move() {
--- Projects/P3/tests/TestGhostValidMoves.java
+++ Projects/P3/tests/TestGhostValidMoves.java
@@ -5,27 +5,6 @@ import java.io.*;
 public class TestGhostValidMoves extends TestCase {
 
 	public void testGhostValidMoves() throws FileNotFoundException{
-		//Creating A Map
-		Mainframe frame = new MainFrame(); //Creates A New Map With Walls and Tokens Initialized
-		
-		//Creating Players
-		Ghost ghost1 = frame.addGhost(new Location(1, 2), "gibby", Color.red); //Creates a red ghost named "name" at location x,y
-		Ghost ghost2 = frame.addGhost(new Location(2, 2), "bibby", Color.blue); //Creates a red ghost named "name" at location x,y
-		Ghost ghost3 = frame.addGhost(new Location(2, 1), "dibby", Color.pink); //Creates a red ghost named "name" at location x,y
-		Ghost ghost4 = frame.addGhost(new Location(2, 3), "wibby", Color.green); //Creates a red ghost named "name" at location x,y
-		PacMan pacman = frame.addPacMan(new Location(3, 2)); //Creates PacMan at location x, y
-
-		//alternatively if you don't need the PacMan or Ghost objects in your tests
-		frame.initPlayers(); //Creates all of the players
-
-		//Start The Game
-		frame.startGame();
-
-		ArrayList<Location> expected = new ArrayList<Location>();
-		expected.add(new Location(1,2));
-		expected.add(new Location(3,2));
-		expected.add(new Location(2,1));
-		expected.add(new Location(2,3));
-		assertEquals(expected, ghost2.get_valid_moves())
+		return null;
 	}
 }
