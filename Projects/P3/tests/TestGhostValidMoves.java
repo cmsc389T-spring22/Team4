960017799df8a55f40a13b974f6475ed764f688c
Soyoung Hong <kirbysoyoung@gmail.com>
2022-03-03 10:22:02 -0500

    ghost - get_valid_moves and test pt 2

--- Projects/P3/tests/TestGhostValidMoves.java
+++ Projects/P3/tests/TestGhostValidMoves.java
@@ -9,11 +9,11 @@ public class TestGhostValidMoves extends TestCase {
 		Mainframe frame = new MainFrame(); //Creates A New Map With Walls and Tokens Initialized
 		
 		//Creating Players
-		Ghost ghost1 = frame.addGhost(new Location(1, 2), "gibby", Color.red); //Creates a red ghost named "name" at location x,y
-		Ghost ghost2 = frame.addGhost(new Location(2, 2), "bibby", Color.blue); //Creates a red ghost named "name" at location x,y
-		Ghost ghost3 = frame.addGhost(new Location(2, 1), "dibby", Color.pink); //Creates a red ghost named "name" at location x,y
-		Ghost ghost4 = frame.addGhost(new Location(2, 3), "wibby", Color.green); //Creates a red ghost named "name" at location x,y
-		PacMan pacman = frame.addPacMan(new Location(3, 2)); //Creates PacMan at location x, y
+		Ghost ghost1 = frame.addGhost(new Location(2, 3), "gibby", Color.red); //Creates a red ghost named "name" at location x,y
+		Ghost ghost2 = frame.addGhost(new Location(3, 3), "bibby", Color.blue); //Creates a red ghost named "name" at location x,y
+		Ghost ghost3 = frame.addGhost(new Location(4, 3), "dibby", Color.pink); //Creates a red ghost named "name" at location x,y
+		Ghost ghost4 = frame.addGhost(new Location(3, 2), "wibby", Color.green); //Creates a red ghost named "name" at location x,y
+		PacMan pacman = frame.addPacMan(new Location(3, 4)); //Creates PacMan at location x, y
 
 		//alternatively if you don't need the PacMan or Ghost objects in your tests
 		frame.initPlayers(); //Creates all of the players
