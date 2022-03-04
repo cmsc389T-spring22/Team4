1b7fbccfab4fa0c347616c125b737b353ca76f1b
jzohdi <31998568+jzohdi@users.noreply.github.com>
2022-03-04 19:00:20 +0100

    added not in range case for ghost - is pacman in range

--- Projects/P3/tests/TestPacManInRange.java
+++ Projects/P3/tests/TestPacManInRange.java
@@ -18,4 +18,17 @@ public class TestPacManInRange extends TestCase {
 		frame.startGame();
 		assertTrue(ghost.isPacManInRange());
 	}
+
+	@Test
+	public void testPacManNotInrange() throws FileNotFoundException {
+		Mainframe frame = new MainFrame(); //Creates A New Map With Walls and Tokens Initialized
+
+		//Creating Players
+		Ghost ghost = frame.addGhost(new Location(5, 5), "Ghost", Color.white); //Creates PacMan at location x, y
+		frame.addPacMan(new Location(6, 2));
+
+		//Start The Game
+		frame.startGame();
+		assertFalse(ghost.isPacManInRange());
+	}
 }
