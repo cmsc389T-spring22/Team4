83eba429bbaac89313e5108f71a1fd531388164b
jzohdi <31998568+jzohdi@users.noreply.github.com>
2022-03-04 18:58:58 +0100

    added is pacman in range to ghost with basic test

--- Projects/P3/tests/TestPacManInRange.java
+++ Projects/P3/tests/TestPacManInRange.java
@@ -1,10 +1,21 @@
 import junit.framework.*;
+import org.junit.Test;
+
 import java.awt.Color;
 import java.io.*;
 
 public class TestPacManInRange extends TestCase {
 
+	@Test
 	public void testPacManInRange() throws FileNotFoundException{
-		return null;
+		Mainframe frame = new MainFrame(); //Creates A New Map With Walls and Tokens Initialized
+
+		//Creating Players
+		Ghost ghost = frame.addGhost(new Location(5, 5), "Ghost", Color.white); //Creates PacMan at location x, y
+		frame.addPacMan(new Location(4, 5));
+
+		//Start The Game
+		frame.startGame();
+		assertTrue(ghost.isPacManInRange());
 	}
 }
