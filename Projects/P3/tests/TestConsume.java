f40c5e8891d259281dfbf0f3770a7c22db04e9ce
nehashijo <nshijo@terpmail.umd.edu>
2022-03-04 15:25:00 -0500

    Test for pacman consume

--- Projects/P3/tests/TestConsume.java
+++ Projects/P3/tests/TestConsume.java
@@ -5,6 +5,20 @@ import java.io.*;
 public class TestConsume extends TestCase {
 
 	public void testConsume() throws FileNotFoundException {
-		return null;	
+		Mainframe frame = new MainFrame(); //Creates A New Map With Walls and Tokens Initialized
+
+		//Creating Players
+		Ghost ghost = frame.addGhost(new Location(1, 0), "test", Color.red); //Creates a red ghost named "name" at location x,y
+
+		//Start The Game
+		frame.startGame();
+
+		if (map.getLoc(new Location(1,0))== Map.Type.COOKIE) {
+			assertNotNull(pacman.consume());
+		} else {
+			assertNull(pacman.consume());
+		}
+
+
 	}
 }
