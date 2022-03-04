68cdce1c9edd6605ccc56c02d005d5fdb042684f
nehashijo <nshijo@terpmail.umd.edu>
2022-03-04 17:11:38 -0500

    Test compiles but is not passing

--- Projects/P3/tests/TestAttack.java
+++ Projects/P3/tests/TestAttack.java
@@ -5,14 +5,16 @@ import java.io.*;
 public class TestAttack extends TestCase {
 
 	public void testAttack() throws FileNotFoundException{
-		Mainframe frame = new MainFrame(); //Creates A New Map With Walls and Tokens Initialized
+		NoFrame frame = new NoFrame();
 
 		//Creating Players
-		Ghost ghost = frame.addGhost(new Location(6, 6), "Ghost", Color.white);
-		frame.addPacMan(new Location(6, 5));
+		Ghost ghost = frame.addGhost(new Location(5, 5), "Ghost", Color.white);
+		frame.addPacMan(new Location(4, 5));
 
 		//Start The Game
 		frame.startGame();
 		assertTrue(ghost.attack());
 	}
 }
+
+
