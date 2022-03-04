9dd69b8fc69effd647a27478dbfcb0c749e98456
nehashijo <nshijo@terpmail.umd.edu>
2022-03-04 18:09:45 -0500

    fixed function

--- Projects/P3/src/Ghost.java
+++ Projects/P3/src/Ghost.java
@@ -25,6 +25,11 @@ public class Ghost{
 	}
 
 	public boolean attack() {
-		return is_pacman_in_range();
+		if (is_pacman_in_range()) {
+			myMap.attack(myName);
+			return true;
+		} else {
+			return false;
+		}
 	}
 }
