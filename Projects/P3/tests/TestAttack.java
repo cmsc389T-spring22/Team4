19784d9d87482975491d12e703b6f816cfb66d01
nehashijo <nshijo@terpmail.umd.edu>
2022-03-04 15:33:16 -0500

    Testing attack function of ghost

--- Projects/P3/tests/TestAttack.java
+++ Projects/P3/tests/TestAttack.java
@@ -5,6 +5,14 @@ import java.io.*;
 public class TestAttack extends TestCase {
 
 	public void testAttack() throws FileNotFoundException{
-		return null;	
+		Mainframe frame = new MainFrame(); //Creates A New Map With Walls and Tokens Initialized
+
+		//Creating Players
+		Ghost ghost = frame.addGhost(new Location(6, 6), "Ghost", Color.white);
+		frame.addPacMan(new Location(6, 5));
+
+		//Start The Game
+		frame.startGame();
+		assertTrue(ghost.attack());
 	}
 }
